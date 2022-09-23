be37e64ba03502e978d5b6e776c7a539c7be3169
matheus <matheus.dresch02@gmail.com>
2022-09-22 21:09:49 -0300

    Updated example.php

--- example.php
+++ example.php
@@ -1,15 +1,30 @@
 <?php
 
-require __DIR__ . '/./vendor/autoload.php';
+require __DIR__ . '/vendor/autoload.php';
 
 use Powerclonic\BetterConsoleColor\ConsoleColor;
 
-$consoleColor = new ConsoleColor(true, true);
+// You can pass 2 params
+// Enable 256 colors, true by default
+// Enable PHP_EOL, true by default
+$consoleColor = new ConsoleColor();
 
-echo $consoleColor->blueBg('Oi');
-echo $consoleColor->yellowText('amarelo');
-echo $consoleColor->lightYellowText('Oi');
-echo $consoleColor->redBg('Oi');
-echo $consoleColor->lightRedBg('Oi');
-echo $consoleColor->lightYellowBg('Oi');
-echo $consoleColor->applyCustomColor(228, 'Oi', true);
+// To color a text:
+$consoleColor->redText('Hello World!');
+
+// To color the text background
+$consoleColor->redBg('Hello World!');
+
+// If you want to use a custom color (256)
+// It will color the text by default, you can color the background
+// defining the last parameter as true
+$consoleColor->applyCustomColor('198', 'Hello World!', false);
+
+// To get a list of the default colors (that come with the package)
+$consoleColor->showAllDefaultColors();
+
+// To get all the 512 colors (Fg/Bg)
+$consoleColor->showAll256Colors();
+
+// You can format the text too
+$consoleColor->bold('Hello World!');
